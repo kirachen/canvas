5f1783dfd2be42e6cc76e7a5606d7930355c3f2c
jipleam <mihai@jiplea.com>
2015-02-18 22:39:57 +0000

    Adding views to display the projects

--- app/controllers/icl_project_portal_controller.rb
+++ app/controllers/icl_project_portal_controller.rb
@@ -1,7 +1,8 @@
 class IclProjectPortalController < ApplicationController
   def show
   	@projects = IclProject.all
-  	#Generate the view that displays all the projects
+  	#Generate the view that displays all the projects + differentiate between students and teachers
+  	
   end
 
 
